3cba81d7db55d562986e1dbe834480795e0a9802
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2024-08-31 22:49:43 +0700

    (add) - materi muttable and immutable variable

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+    // muttable variable (variabel yang bisa di isikan nilai kembali)
+    var name: String = "bayu";
+    name = "firmansyah";
+
+    // template literal
+    println("Nama saya adalah ${name}");
+
+
+    // immutable variable (variabel yang tidak bisa di isikan nilai kembali)
+    val phi: Double = 3.14;
+
+    // templare literal
+    println("Nilai dari phi adalah ${phi}")
 }
