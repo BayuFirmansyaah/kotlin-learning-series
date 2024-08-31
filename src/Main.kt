0847b82452879806d02dac73b39245a3cd05b988
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2024-08-31 22:58:28 +0700

    (add) - materi range in kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
+    println("=================================================")
+
     // muttable variable (variabel yang bisa di isikan nilai kembali)
     var name: String = "bayu";
     name = "firmansyah";
@@ -12,4 +14,31 @@ fun main() {
 
     // templare literal
     println("Nilai dari phi adalah ${phi}")
+    println("=================================================")
+
+    // type data range (di tandai dengan doubel colon atau ..)
+    var range = 1..10
+    println(range);
+
+    // looping range
+    for(num in range){
+        print("${num},");
+    }
+
+    // add new line
+    println();
+
+    // first value from range
+    println("first value in range ${range.first()}")
+
+    // last value from range
+    println("last value in range ${range.last()}")
+
+    // contains value from range
+    println("is exist value 20 in range 1..10 is ${range.contains(20)}")
+
+    // step value from range
+    println("step value in range ${range.step(5)}")
+
+    println("=================================================")
 }
