df0bdba84e8135ba4e4e53058b24e77aa19b4d2e
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2024-08-31 23:44:26 +0700

    (add) - function return type integer

--- src/Function.kt
+++ src/Function.kt
@@ -1,7 +1,12 @@
+// return UNIT digunakan untuk function yang tidak melakukan return value
 fun biodata(name: String, age: Int, isMale: Boolean): Unit{
     println("Nama saya adalah ${name}")
     println("Saya sekarang berumur ${age}")
     println("Dan saya ${ if(isMale) "seorang" else "bukan seorang" } laki-laki")
+}
+
+fun sum(first: Int, second: Int): Int{
+    return first+second
 }
 
 fun main()
@@ -11,6 +16,8 @@ fun main()
         name = "Bayu Firmansyah",
         age = 21
     )
+
+    println("Hasil penjumlahan dari 2 dan 3 = ${sum(2, 3)}")
 }
 
 
